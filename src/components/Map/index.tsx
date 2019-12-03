77ba6437affe4ef55184d2967fd89e194d7a569a
José Abarca Saavedra <jose@smart.rancagua.cl>
2019-12-03 10:56:29 -0300

    mapas

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './styles.css'
 
 const Map: React.FC = () => {
-	return <div className='Map'>Hola soy el Map</div>
+	return (
+		<div className='Map mt-5'>
+			<h4>Lugares de Votación</h4>
+			<img className='img-fluid' src='assets/mapa.png' />
+		</div>
+	)
 }
 
 export default Map
