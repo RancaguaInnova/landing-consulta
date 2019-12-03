2ae50fcc6c92f2c2aab2b65916429d052d0fc1c5
José Abarca Saavedra <jose@smart.rancagua.cl>
2019-12-03 10:23:37 -0300

    demo image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './styles.css'
 
 const Loading: React.FC = () => {
-	return <div className='Loading'>TITULO</div>
+	return (
+		<div className='header'>
+			<h1>Consulta Ciudadana </h1>
+			<h2>Rancagua 2019</h2>
+		</div>
+	)
 }
 
 export default Loading
--- src/components/Vote/index.tsx
+++ src/components/Vote/index.tsx
@@ -4,9 +4,18 @@ import './styles.css'
 const Vote: React.FC = () => {
 	return (
 		<div>
-			<h3>Voto1</h3>
-			<h3>Voto2</h3>
-			<h3>Voto3</h3>
+			<img
+				src='http://static.t13.cl/images/original/2017/12/1513090211-facsimil-presi-segunda-pdf-page-001.jpg'
+				width='100%'
+			/>
+			<img
+				src='http://static.t13.cl/images/original/2017/12/1513090211-facsimil-presi-segunda-pdf-page-001.jpg'
+				width='100%'
+			/>
+			<img
+				src='http://static.t13.cl/images/original/2017/12/1513090211-facsimil-presi-segunda-pdf-page-001.jpg'
+				width='100%'
+			/>
 		</div>
 	)
 }
